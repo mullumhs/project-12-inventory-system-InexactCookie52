c262608084cf3441f8fd10c238f97744463725c0
InexactCookie52 <140124275+InexactCookie52@users.noreply.github.com>
2024-06-11 14:58:49 +1000

    lab 2 kinda

--- lab2.py
+++ lab2.py
@@ -10,20 +10,36 @@
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 # Step 1: Import the Item class from lab1.py
-
+from lab1 import Item
 class Inventory:
     def __init__(self):
-
+      self._item = []
 # Step 2: Define the InventoryManager class as a facade to handle the inventory operations.
 # It should include methods to add, remove, update, and display items in the inventory.
 
 
-    def add_item(self, item):
-
-        print("")
-
-
-    def update_price(self, item, new_price):
+    def add_item(self, name, price, qty):
+        for item in self._item:
+            if item.name == name:
+                print(f"Item, {item.name} already exits")
+                return
+            new_item = item(name, price, qty)
+            self._item.append(new_item)
+            print(f"item, {item.name} has been added:")
+        
+    def delete_item(self, name):
+        item_selection = input(f"What item would you like to delete? ")
+        for item in self._item:
+            if item.name == item_selection:
+                self._item.remove
+                print(f"Item, {item_selection} has been removed:")
+                return
+        print(f"Item, {item_selection} is not in the list")
+    def print_list(self, name, price, qty):
+        for item in self._item:
+            print (f"{item.name}, {item.price}, {item.qty}:")
+            
+    def update_item(self, name, price, qty):
 
         print("")
 
